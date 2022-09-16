8d281ae843022d336f38d8581f7421ab02d4e856
= <motasem.yakhni@gmail.com>
2022-09-16 18:01:13 +0300

    add winning comditions for both sides

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
 const boxes = document.querySelectorAll('.box')
 const resetBtn = document.getElementById('reset')
+const displayWinner = document.getElementById('display-winner')
 const player1Turn = 0
 const player2Turn = 1
 let turn = player1Turn
+const displayP1Score = document.getElementById('p1')
+const displayP2Score = document.getElementById('p2')
+let p1Score = 0
+let p2Score = 0
 
-resetBtn.addEventListener('click', () => {
-    const redImages = document.querySelectorAll('.red')
-    const yellowImages = document.querySelectorAll('.yellow')
+const y1 = document.querySelector('.y-1')
+const y2 = document.querySelector('.y-2')
+const y3 = document.querySelector('.y-3')
+const y4 = document.querySelector('.y-4')
+const y5 = document.querySelector('.y-5')
+const y6 = document.querySelector('.y-6')
+const y7 = document.querySelector('.y-7')
+const y8 = document.querySelector('.y-8')
+const y9 = document.querySelector('.y-9')
 
-    redImages.forEach(redImage => redImage.classList.add('view-hidden'))
-    yellowImages.forEach(yellowImage => yellowImage.classList.add('view-hidden'))
-})
+const r1 = document.querySelector('.r-1')
+const r2 = document.querySelector('.r-2')
+const r3 = document.querySelector('.r-3')
+const r4 = document.querySelector('.r-4')
+const r5 = document.querySelector('.r-5')
+const r6 = document.querySelector('.r-6')
+const r7 = document.querySelector('.r-7')
+const r8 = document.querySelector('.r-8')
+const r9 = document.querySelector('.r-9')
+
+const declareYellowWinner = "Yellow Won!"
+const declareRedWinner = "Red Won!"
 
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
@@ -25,13 +45,129 @@ boxes.forEach((box) => {
                return // if yellow here we cant put red
             }
             redCoin.classList.remove('view-hidden')
+            redCoin.classList.add('red')
             turn = player2Turn
         }else {
             if(!redCoin.classList.contains('view-hidden')) {
                 return //if red here we cant put yellow
              }
             yellowCoin.classList.remove('view-hidden')
+            yellowCoin.classList.add('yellow')
             turn = player1Turn
-        }     
+        }
+
+        // For Red
+        // total winning conditions => 8 cases:
+        // winning by rows => 3 cases
+        if(r1.classList.contains('red') && r2.classList.contains('red') && r3.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }else if(r4.classList.contains('red') && r5.classList.contains('red') && r6.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }else if(r7.classList.contains('red') && r8.classList.contains('red') && r9.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }
+        // winning br columns => 3 cases
+        else if(r1.classList.contains('red') && r4.classList.contains('red') && r7.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }else if(r2.classList.contains('red') && r5.classList.contains('red') && r8.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }else if(r3.classList.contains('red') && r6.classList.contains('red') && r9.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }
+        // winning br diagonals
+        else if(r1.classList.contains('red') && r5.classList.contains('red') && r9.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        }else if(r3.classList.contains('red') && r5.classList.contains('red') && r7.classList.contains('red')) {
+            displayWinner.textContent = declareRedWinner
+            p1Score++
+            displayP1Score.textContent = p1Score
+            return
+        } 
+
+        //For yellow
+        // total winning conditions => 8 cases:
+        // winning by rows => 3 cases
+        if(y1.classList.contains('yellow') && y2.classList.contains('yellow') && y3.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }else if(y4.classList.contains('yellow') && y5.classList.contains('yellow') && y6.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }else if(y7.classList.contains('yellow') && y8.classList.contains('yellow') && y9.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }
+        // winning by columns => 3 cases
+        else if(y1.classList.contains('yellow') && y4.classList.contains('yellow') && y7.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }else if(y2.classList.contains('yellow') && y5.classList.contains('yellow') && y8.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }else if(y3.classList.contains('yellow') && y6.classList.contains('yellow') && y9.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }
+        // winning by diagonals
+        else if(y1.classList.contains('yellow') && y5.classList.contains('yellow') && y9.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }else if(y3.classList.contains('yellow') && y5.classList.contains('yellow') && y7.classList.contains('yellow')) {
+            displayWinner.textContent = declareYellowWinner
+            p2Score++
+            displayP2Score.textContent = p2Score
+            return
+        }
     })
+})
+
+resetBtn.addEventListener('click', () => {
+    const redImages = document.querySelectorAll('.red-images')
+    const yellowImages = document.querySelectorAll('.yellow-images')
+
+    redImages.forEach(redImage => {
+        redImage.classList.add('view-hidden')
+        redImage.classList.remove('red')
+    })
+    yellowImages.forEach(yellowImage => {
+        yellowImage.classList.add('view-hidden')
+        yellowImage.classList.remove('yellow')
+    })
+    turn = player1Turn
+    displayWinner.textContent = null
 })
