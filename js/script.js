4a0dd9142c35caa270d990053e44eec76b413220
= <motasem.yakhni@gmail.com>
2022-09-16 16:06:27 +0300

    add reset btn

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 const boxes = document.querySelectorAll('.box')
+const resetBtn = document.getElementById('reset')
 const player1Turn = 0
 const player2Turn = 1
 let turn = player1Turn
+
+resetBtn.addEventListener('click', () => {
+    const redImages = document.querySelectorAll('.red')
+    const yellowImages = document.querySelectorAll('.yellow')
+
+    redImages.forEach(redImage => redImage.classList.add('view-none'))
+    yellowImages.forEach(yellowImage => yellowImage.classList.add('view-none'))
+})
 
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
         const redCoin = box.firstChild.nextSibling
         const yellowCoin = box.lastChild.previousSibling
 
-        console.log(redCoin)
-        console.log(yellowCoin)
+        // console.log(redCoin)
+        // console.log(yellowCoin)
 
         if(turn === player1Turn) {
             if(!yellowCoin.classList.contains('view-none')) {
@@ -23,7 +32,6 @@ boxes.forEach((box) => {
              }
             yellowCoin.classList.remove('view-none')
             turn = player1Turn
-        }
-        
+        }     
     })
 })
